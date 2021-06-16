cbb17c912181a103cc3bf3e682f5b0ff349c7098
Joon Im <73262010+joonhim@users.noreply.github.com>
2021-06-15 23:42:49 -0700

    Update ad_hoc_team.java

--- src/main/java/csulb/cecs323/model/ad_hoc_team.java
+++ src/main/java/csulb/cecs323/model/ad_hoc_team.java
@@ -1,21 +1,19 @@
 package csulb.cecs323.model;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+
+@DiscriminatorValue("Ad Hoc Team")
 public class ad_hoc_team extends authoring_entities {
     private String email = getEmail();
     private String type;
 
     public ad_hoc_team(){}
-    public ad_hoc_team (String email, String name, String type){
+    public ad_hoc_team (String email, String name){
         super(email, name);
-        this.type = type;
     }
 
     @Override
